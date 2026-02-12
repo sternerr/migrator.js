818d0e936884f9911148b63080756642d84acd64
Noah <volpy2000@gmail.com>
2026-02-12 20:43:05 +0100

    feat: add possibility to run up and down sql scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("I hope i don't have skill issues for this project");
+import fs from "fs";
+import path from "path";
+import pg from "pg";
+import parseSqlStatements from "./core/parser/parser.mts";
+import { error } from "console";
+
+const MIGRATIONS_DIR = path.join(process.cwd(), "migrations");
+
+async function main() {
+    const file: string = process.argv[2];
+    const connectionString: string = process.argv[3];
+
+    const client: pg.Client = new pg.Client({
+        connectionString
+    });
+
+    await client.connect();
+    fs.readFile(path.join(MIGRATIONS_DIR, file), async (err, buffer: Buffer) => {
+        if(err) {
+            throw new Error("unable to read file: " + file);
+        }
+
+        const stmts = parseSqlStatements(buffer);
+        console.log(stmts);
+        
+        for(const s of stmts) {
+            await client.query(s);
+        }
+
+        await client.end();
+    });
+}
+
+main();
